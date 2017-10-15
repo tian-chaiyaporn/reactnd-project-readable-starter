477417d97cd0663063f2fb63bf97cee576d6a322
tian <tian_chaiya12@hotmail.com>
2017-10-15 18:53:00 +0700

    add initial state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import './App.css';
 import CategoryList from './CategoryList'
+import PostList from './PostList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CategoryList categories={['a', 'b', 'c']} />
+        <CategoryList categories={this.props.categories} />
+        <PostList posts={this.props.posts} />
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps ({categories, posts}) {
+  return {
+    categories: categories,
+    posts: posts
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,7 +2,64 @@ import {
   ADD_COMMENT
 } from '../actions'
 
-function reducer(state = {}, action) {
+const initialState = {
+  categories: [
+    {
+      name: 'test',
+      path: 'test'
+    },
+    {
+      name: 'test2',
+      path: 'test2'
+    }
+  ],
+  posts: [
+    {
+      id: '8xf0y6ziyjabvozdd253nd',
+      timestamp: 1467166872634,
+      title: 'Udacity is the best place to learn React',
+      body: 'Everyone says so after all.',
+      author: 'thingtwo',
+      category: 'react',
+      voteScore: 6,
+      deleted: false
+    },
+    {
+      id: '6ni6ok3ym7mf1p33lnez',
+      timestamp: 1468479767190,
+      title: 'Learn Redux in 10 minutes!',
+      body: 'Just kidding. It takes more than 10 minutes to learn technology.',
+      author: 'thingone',
+      category: 'redux',
+      voteScore: -5,
+      deleted: false
+    }
+  ],
+  comments: [
+    {
+      id: '8tu4bsun805n8un48ve89',
+      parentId: "8xf0y6ziyjabvozdd253nd",
+      timestamp: 1469479767190,
+      body: 'Comments. Are. Cool.',
+      author: 'thingone',
+      voteScore: -5,
+      deleted: false,
+      parentDeleted: false
+    },
+    {
+      id: '8tu4bsun805n8un48ve89',
+      parentId: "8xf0y6ziyjabvozdd253nd",
+      timestamp: 1469479767190,
+      body: 'Comments. Are. Cool.',
+      author: 'thingone',
+      voteScore: -5,
+      deleted: false,
+      parentDeleted: false
+    }
+  ]
+}
+
+function reducer(state = initialState, action) {
   switch (action.type) {
     case ADD_COMMENT :
       return {
