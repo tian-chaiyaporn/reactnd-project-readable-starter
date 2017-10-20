a1a86ae3348db4d0635a62c3ad9552ea04773902
tian <tian_chaiya12@hotmail.com>
2017-10-20 21:43:05 +0700

    add sort mechanism

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,1 @@
-Plan:
 
-- List pages (root or category) include a mechanism for sorting by date or by score (at a minimum) and the sort works properly.
-- User is able to navigate between categories, main page and post detail pages without typing address in the address bar.
--- frontend/src/PostList.js
+++ frontend/src/PostList.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PostItem from './PostItem'
 
-export default function PostList ({posts}) {
-  const postItems = posts.map(post => {
-    const date = new Date(post.timestamp)
-    return (
-      <li key={post.id}>
-        <PostItem
-          id={post.id}
-          postTitle={post.title}
-          postAuthor={post.author}
-          postDate={date.toString()}
-          postBody={post.body}
-          currentScore={post.voteScore}
-        />
-      </li>
-    )
-  })
+class PostList extends Component {
+  constructor() {
+    super()
+    this.state = {sort: ''}
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  return (
-    <ul className="post-list">
-      {postItems}
-    </ul>
-  )
+  handleChange(e) {
+    this.setState({sort: e.target.value})
+  }
+
+  render() {
+    switch (this.state.sort) {
+      case 'date':
+        this.props.posts.sort((a, b) => b.timestamp - a.timestamp)
+        break;
+      case 'vote':
+        this.props.posts.sort((a, b) => b.voteScore - a.voteScore)
+        break;
+      default :
+        this.props.posts.sort((a, b) => b.timestamp - a.timestamp)
+    }
+    const postItems = this.props.posts.map(post => {
+      const date = new Date(post.timestamp)
+      return (
+        <li key={post.id}>
+          <PostItem
+            id={post.id}
+            postTitle={post.title}
+            postAuthor={post.author}
+            postDate={date.toString()}
+            postBody={post.body}
+            currentScore={post.voteScore}
+          />
+        </li>
+      )
+    })
+
+    return (
+      <div className="post-list-container">
+        <select name="sort" onChange={this.handleChange}>
+          <option value="date">date</option>
+          <option value="vote">vote</option>
+        </select>
+        <ul className="post-list">
+          {postItems}
+        </ul>
+      </div>
+    )
+  }
 }
+
+export default PostList
